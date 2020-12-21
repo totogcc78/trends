27f54f5a7413efaaddda92bd4e1d1ad76fb8fc32
totogcc78 <totogcc@hotmail.com>
2020-12-21 17:55:02 +0000

    updated table

--- forex_screener_scrap.py
+++ forex_screener_scrap.py
@@ -5,50 +5,53 @@ from bs4 import BeautifulSoup
 import time
 
 def get_platinum_screener_list():
-	result = {}
-	while len(result) ==  0:
-		url = "https://www.tradingview.com/forex-screener/"
-		options = Options()
-	# options.add_argument("start-maximized")
-		options.add_argument("--headless")
-		options.add_argument("--window-size=1920x1080")
-		driver = webdriver.Chrome(executable_path=executable_path=, options=options)
-		driver.get(url)
+    result = {}
+    while len(result) ==  0:
+        url = "https://www.tradingview.com/forex-screener/"
+        options = Options()
+    # options.add_argument("start-maximized")
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1920x1080")
 
-		data = driver.find_element_by_xpath('//*[@id="js-screener-container"]/div[4]/table/tbody').text
+        driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", options=options)
+        driver.get(url)
 
-		driver.quit()
+        data = driver.find_element_by_xpath('//*[@id="js-screener-container"]/div[4]/table/tbody').text
 
-		list_ticker = []
-		list_currency = []
-		list_rating = []
+        driver.quit()
 
-		data = data.splitlines()
-		start_pt = 0
-		end = len(data)
-		while start_pt < end:
-			# print("***")
-			line_1 = data[start_pt]
+        list_ticker = []
+        list_currency = []
+        list_rating = []
 
-			line_2 = data[start_pt+1]
-		
-			line_3 = data[start_pt+2]
-			line_3 = line_3.split()
-			line_3 = line_3[-1]
-		
-			list_ticker.append(line_1)
-			list_currency.append(line_2)
-			list_rating.append(line_3)
-		
-			start_pt = start_pt + 3
+        data = data.splitlines()
+        start_pt = 0
+        end = len(data)
+        while start_pt < end:
+            # print("***")
+            line_1 = data[start_pt]
 
-
-#	result = {}
-		for i in range(len(list_ticker)):
-			key = list_ticker[i][:3] + '/' + list_ticker[i][3:]
-			result[key] = list_rating[i]
+            line_2 = data[start_pt+1]
+        
+            line_3 = data[start_pt+2]
+            line_3 = line_3.split()
+            line_3 = line_3[-1]
+        
+            list_ticker.append(line_1)
+            list_currency.append(line_2)
+            list_rating.append(line_3)
+        
+            start_pt = start_pt + 3
 
 
-		print(len(result), ' recieved from forex screener')
+#   result = {}
+        for i in range(len(list_ticker)):
+            key = list_ticker[i][:3] + '/' + list_ticker[i][3:]
+            result[key] = list_rating[i]
 
-	return result
+
+        print(len(result), ' recieved from forex screener')
+
+    return result
+
+get_platinum_screener_list()
